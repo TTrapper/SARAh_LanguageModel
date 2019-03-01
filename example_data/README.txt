bfe7b117768aa6bfb9f36948cb4d8fa64e3f9008
TTrapper <mike.sk.traynor@gmail.com>
2019-02-28 21:44:22 -0400

    data is now fixed number of chars per line

--- example_data/README.txt
+++
@@ -1,5 +0,0 @@
-Data should be ascii or otherwise encode 1 byte per character: Tensorflow's string_split doesn't play nice otherwise.
-Words are assumed to be be delimited by a single space character.
-The source/target pairs are selected by choosing concurrent lines, so each line is considered the target for the previous line.
-Lines may be clipped to a maximum length (number of words). This length is specified in the configuration file.
-Words may be clipped to a maximum length (number of characters). This length is specified in the configuration file.
--- prepare_data.py
+++ prepare_data.py
@@ -6,33 +6,21 @@ import unicodedata
 parser = argparse.ArgumentParser()
 parser.add_argument('--src', type=str, required=True)
 parser.add_argument('--dst', type=str, required=True)
-parser.add_argument('--seq_len', type=int, default=32)
-parser.add_argument('--go_stop_chr', type=str, default=chr(0))
+parser.add_argument('--seq_len', type=int, default=128)
+parser.add_argument('--newline_replacement', type=str, default=' ][ ')
 args = parser.parse_args()
 assert(args.src != args.dst)
-
-
-def parse_by_alpha(line):
-    """
-    Parses by whitespace, then each word is split into groups of alphas and non-alphas
-    ex: "don't do that please.": [don ' t do that please .]
-        "http://www.bs.com/184": [http :// www . bs . com /184]
-    """
-    words = []
-    for word in line.strip().split():
-        words.extend(re.split('([^a-zA-Z]+)', word))
-    # The above splits add null strings, remove them.
-    return [word for word in words if word != '']
 
 if not os.path.exists(args.dst):
     os.makedirs(args.dst)
 
 for src_doc in os.listdir(args.src):
-    with open('{}/{}'.format(args.dst, src_doc), 'w') as dst_doc, open('{}/{}'.format(args.src, src_doc), 'r') as src_doc:
-        src_doc = src_doc.read().decode('utf-8').encode('ascii', 'ignore')
-        if args.go_stop_chr in src_doc:
-            raise ValueError("The token reserved for EOS/SOS was found in the doc")
-        src_doc = src_doc.replace('\n', '_-_').replace('\r', '_-_')
-        src_doc = parse_by_alpha(src_doc)
-        src_doc = [' '.join(src_doc[i:i + args.seq_len]) for i in xrange(0, len(src_doc), args.seq_len)]
+    with open('{}/{}'.format(args.dst, src_doc), 'wb') as dst_doc, open('{}/{}'.format(args.src, src_doc), 'r') as src_doc:
+        src_doc = src_doc.read().decode('utf-8').encode('ascii', 'ignore') # TODO Loss of information on ascii encode
+        src_doc = src_doc.replace('\n', args.newline_replacement).replace('\r', args.newline_replacement)
+        # Split doc into seq_len sized chunks
+        num_chars_range = '.{{{},{}}}'.format(1, args.seq_len)
+        src_doc = re.findall(num_chars_range, src_doc)
+        # Pad the last string to seq_len
+        src_doc[-1] = src_doc[-1].ljust(args.seq_len)
         dst_doc.write('\n'.join(src_doc))
