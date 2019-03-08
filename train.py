a7f85b5aaec2fb24c1b33f8dee834725b4b63eee
TTrapper <mike.sk.traynor@gmail.com>
2019-03-07 22:29:09 -0400

    update to train script for 2D data with fixedlen words

--- train.py
+++ train.py
@@ -13,40 +13,16 @@ import config
 
 def build_model(data, conf, reuse=False):
     with tf.variable_scope('Model', dtype=tf.float32, reuse=reuse) as scope:
-        model = model_def.Model(data.src,
-            data.src_sentence_len,
-            data.trg_word_enc,
-            data.trg_sentence_len,
-            data.trg_word_dec,
-            data.trg_word_len,
-            len(data.chr_to_freq),
-            conf)
+        model = model_def.Model(data.src, data.trg, len(data.chr_to_freq), 0, conf)
         scope.reuse_variables()
         # Create a copy of the model that operates over the inference pipeline
-        conf = config.generate_config(1.0)
-        free_model = model_def.Model(data.src_inference,
-            data.src_sentence_len_inference,
-            data.trg_word_enc_inference,
-            data.trg_sentence_len_inference,
-            data.trg_word_dec_inference,
-            data.trg_word_len_inference,
-            len(data.chr_to_freq),
-            conf)
+        conf = config.get_config(keep_prob=1.0)
+        free_model = model_def.Model(data.free_src, data.free_trg, len(data.chr_to_freq), 0, conf)
     return model, free_model
 
-def calc_loss(logits, targets, word_lens, sentence_lens):
-    mask = tf.where(tf.equal(targets, -1), tf.zeros_like(targets), tf.ones_like(targets))
-    targets *= mask # softmax doesn't like oov targets, turn -1 padding to 0
+def calc_loss(logits, targets):
     loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=targets)
-    loss *= tf.cast(mask, loss.dtype) # remove loss calculated over the padding chars
-    # Don't divide by zero
-    word_lens = tf.where(tf.equal(word_lens, 0), tf.ones_like(word_lens), word_lens)
-    sentence_lens = tf.where(tf.equal(sentence_lens, 0), tf.ones_like(sentence_lens), sentence_lens)
-    # Get cost per char, account for varying sequence lengths
-    loss = tf.reshape(loss, tf.shape(targets))
-    loss = tf.reduce_sum(loss, axis=2) / tf.cast(word_lens, loss.dtype)
-    loss = tf.reduce_sum(loss, axis=1) / tf.cast(sentence_lens, loss.dtype)
-    loss = tf.reduce_mean(loss) # batch mean
+    loss = tf.reduce_mean(loss)
     return loss
 
 def build_train_op(loss, learn_rate, max_grad_norm):
@@ -60,25 +36,20 @@ def run_inference(model, data, conf, sess, softmax_temp=1e-24):
     # TODO: This is very slow because the entire model is rerun for each char prediction
     paths = [args.datadir + p for p in os.listdir(args.datadir)]
     condition = data_pipe.getRandomSentence(paths, numSamples=1, sampleRange=1)[0][0]
-    condition = condition.strip()
+    condition = condition[:-1] # remove newline
     print condition
     result = ''
-    char_idx = 0
-    word_idx = 0
     try:
-        for char_count in range(128):
-            feed = {data.src_place:condition, data.trg_place:result.strip(), model.softmax_temp:softmax_temp}
-            predictions = sess.run(model.predictions_3, feed_dict=feed)
-            next_char = data.id_to_chr[predictions[0, word_idx, char_idx]]
-            if next_char == data.go_stop_token:
-                if char_idx == 0: # EOS
-                    print result
-                    return
-                next_char = ' '
-                word_idx += 1
-                char_idx = -1
+        for char_count in range(len(condition)):
+            # The number of chars must be a multiple of chrs_per_word. Pad the result
+            padlen = conf['chrs_per_word'] - (len(result) % conf['chrs_per_word'])
+            padstr = ''.ljust(padlen)
+            feed = {data.src_place:condition,
+                    data.trg_place:result + padstr,
+                    model.softmax_temp:softmax_temp}
+            predictions = sess.run(model.predictions_2, feed_dict=feed)
+            next_char = data.id_to_chr[predictions[0, char_count]]
             result += next_char
-            char_idx += 1
         print result
     except Exception as e:
         print 'Inference failed: '
@@ -86,22 +57,25 @@ def run_inference(model, data, conf, sess, softmax_temp=1e-24):
         print result
 
 def train():
-    conf = config.generate_config()
-    data = data_pipe.Data(args.datadir, conf['batch_size'], conf['max_word_len'], conf['max_line_len'])
+    conf = config.get_config()
+    data = data_pipe.Data(args.datadir, conf['batch_size'])
     model, free_model = build_model(data, conf)
-    loss = calc_loss(model.out_logits_4, data.trg, data.trg_word_len, data.trg_sentence_len)
+    loss = calc_loss(model.out_logits_3, data.trg)
     train_op, grads, norm = build_train_op(loss, conf['learn_rate'], conf['max_grad_norm'])
     sess = tf.Session()
     saver = tf.train.Saver(tf.trainable_variables(), max_to_keep=1)
     sess.run(tf.tables_initializer())
     sess.run(tf.global_variables_initializer())
+    if args.restore_checkpoint is not None:
+        print 'restoring checkpoint: ' +  args.restore_checkpoint
+        saver.restore(sess, args.restore_checkpoint)
     data.initialize(sess, data.datadir + '*')
     recent_costs = deque(maxlen=100)
     batch_num = 0
     while True:
         [_,n,c] = sess.run([train_op,norm,loss])
         recent_costs.append(c)
-        if batch_num%250 == 0:
+        if batch_num%25 == 0:
             print batch_num, sum(recent_costs)/len(recent_costs), n
             saver.save(sess, './saves/model.ckpt')
             run_inference(free_model, data, conf, sess)
@@ -112,5 +86,6 @@ def train():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--datadir', type=str, required=True)
+    parser.add_argument('--restore_checkpoint', type=str, default=None)
     args = parser.parse_args()
     train()
