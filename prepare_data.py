99d245072678ba618a422f8a78c1de92f4b7df78
TTrapper <mike.sk.traynor@gmail.com>
2019-04-15 12:39:28 -0300

    data prep only runs if it's __main__

--- prepare_data.py
+++ prepare_data.py
@@ -8,9 +8,6 @@ parser.add_argument('--src', type=str, required=True)
 parser.add_argument('--dst', type=str, required=True)
 parser.add_argument('--seq_len', type=int, default=32)
 parser.add_argument('--go_stop_chr', type=str, default=chr(0))
-args = parser.parse_args()
-assert(args.src != args.dst)
-
 
 def parse_by_alpha(line):
     """
@@ -24,15 +21,19 @@ def parse_by_alpha(line):
     # The above splits add null strings, remove them.
     return [word for word in words if word != '']
 
-if not os.path.exists(args.dst):
-    os.makedirs(args.dst)
 
-for src_doc in os.listdir(args.src):
-    with open('{}/{}'.format(args.dst, src_doc), 'w') as dst_doc, open('{}/{}'.format(args.src, src_doc), 'r') as src_doc:
-        src_doc = src_doc.read()
-        if args.go_stop_chr in src_doc:
-            raise ValueError("The token reserved for EOS/SOS was found in the doc")
-        src_doc = src_doc.replace('\n', ' _-_ ').replace('\r', ' _-_ ')
-        src_doc = parse_by_alpha(src_doc)
-        src_doc = [' '.join(src_doc[i:i + args.seq_len]) for i in xrange(0, len(src_doc), args.seq_len)]
-        dst_doc.write('\n'.join(src_doc))
+if __name__ == '__main__':
+    args = parser.parse_args()
+    assert(args.src != args.dst)
+    if not os.path.exists(args.dst):
+        os.makedirs(args.dst)
+
+    for src_doc in os.listdir(args.src):
+        with open('{}/{}'.format(args.dst, src_doc), 'w') as dst_doc, open('{}/{}'.format(args.src, src_doc), 'r') as src_doc:
+            src_doc = src_doc.read()
+            if args.go_stop_chr in src_doc:
+                raise ValueError("The token reserved for EOS/SOS was found in the doc")
+            src_doc = src_doc.replace('\n', ' _-_ ').replace('\r', ' _-_ ')
+            src_doc = parse_by_alpha(src_doc)
+            src_doc = [' '.join(src_doc[i:i + args.seq_len]) for i in xrange(0, len(src_doc), args.seq_len)]
+            dst_doc.write('\n'.join(src_doc))
