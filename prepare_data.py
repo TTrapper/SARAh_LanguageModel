66b817387ecad5b5918ba10a142f6b3bce7fca25
TTrapper <mike.sk.traynor@gmail.com>
2019-05-09 17:57:59 -0300

    Configurable replacement for newline chars

--- prepare_data.py
+++ prepare_data.py
@@ -12,6 +12,7 @@ parser.add_argument('--seq_len', type=int, default=64)
 parser.add_argument('--parse_sentences', type=str, choices=['yes', 'no'], default='no')
 parser.add_argument('--autoencode', type=str, choices=['yes', 'no'], default='no')
 parser.add_argument('--go_stop_chr', type=str, default=chr(0))
+parser.add_argument('--newline', type=str, default=' _-_ ')
 
 def parse_by_alpha(line):
     """
@@ -37,13 +38,13 @@ if __name__ == '__main__':
             src_doc = src_doc.read()
             if args.go_stop_chr in src_doc:
                 raise ValueError("The token reserved for EOS/SOS was found in the doc")
-            src_doc = src_doc.replace('\n', ' _-_ ').replace('\r', ' _-_ ')
+            src_doc = src_doc.replace('\n', args.newline).replace('\r', args.newline)
             if args.parse_sentences:
-                src_doc = src_doc.decode('utf-8')
+                src_doc = src_doc.decode('utf-8', 'ignore')
                 tokenizer = PunktSentenceTokenizer(src_doc)
                 src_doc = tokenizer.tokenize(src_doc)
                 src_doc = [' '.join(parse_by_alpha(s)) for s in src_doc]
-                src_doc = [s.encode('utf-8') for s in src_doc]
+                src_doc = [s.encode('utf-8', 'ignore') for s in src_doc]
             else:
                 src_doc = parse_by_alpha(src_doc)
                 src_doc = [' '.join(src_doc[i:i + args.seq_len]) for i in xrange(0, len(src_doc), args.seq_len)]
