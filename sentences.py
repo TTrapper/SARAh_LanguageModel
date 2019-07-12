f3d61fb7e468219b42e9f089237dac9da310d83b
TTrapper <mike.sk.traynor@gmail.com>
2019-07-12 16:20:06 -0300

    Use memmap and chunked processing to handle very large embedding matrices

--- sentences.py
+++ sentences.py
@@ -4,7 +4,7 @@ import time
 import sys
 import random
 
-from sklearn.metrics import pairwise_distances
+from sklearn.metrics import pairwise_distances_chunked
 from gensim.models import KeyedVectors
 import numpy as np
 import tensorflow as tf
@@ -27,45 +27,87 @@ parser.add_argument('--project', type=str, default='no', choices=['yes', 'no'],
 parser.add_argument('--batch_size', type=int, default=256,
     help='batch_size to use when encoding sentences')
 
-def get_sentence_embeds(savename, datadir, restore, max_num, batch_size):
-    embed_file = '{}.embeds.npy'.format(savename)
-    if os.path.exists(embed_file):
-        print 'loading precomputed embeddings: {}'.format(embed_file)
-        sentence_embeds = np.load(embed_file)
-        return sentence_embeds
-    sentence_embeds = compute_sentence_embeds(datadir, restore, savename, max_num, batch_size)
-    return sentence_embeds
-
-def run_loop(sess, data, sentence_embeds_2_op, sentences_3_op, max_num):
+def run_loop(sess, save_path, shape_path, sentence_embeds_2_op, max_num):
     batch_num = 0
     num_encoded = 0
-    embeds = []
+    embed_size = sentence_embeds_2_op.shape.as_list()[1]
+    embeds = np.memmap(save_path, dtype=np.float16, mode='w+', shape=(1, embed_size))
     start_time = time.time()
     while True:
-        batch_num += 1
         try:
             sentence_embeds_2 = sess.run(sentence_embeds_2_op)
-            num_encoded += sentence_embeds_2.shape[0]
-            embeds.append(sentence_embeds_2)
+            batch_size = sentence_embeds_2.shape[0]
+            num_encoded += batch_size
+            embeds = np.memmap(save_path, dtype=np.float16, mode='r+', shape=(num_encoded, embed_size))
+            embeds[batch_num*batch_size:, :] = sentence_embeds_2
         except tf.errors.OutOfRangeError as e:
             break
+        except KeyboardInterrupt:
+            print 'Interrupted. Saving shape of embeddings and exiting'
+            np.save(shape_path, np.array([embeds.shape[0], embed_size]))
+            exit()
+        batch_num += 1
         if max_num is not None and num_encoded >= max_num:
             break
         if batch_num%5 == 1:
             sps = num_encoded/(time.time() - start_time)
-            print '{} sentences encoded ({} sentences/sec)\r'.format(num_encoded, sps),
+            print '{} sentences encoded ({} sentences/sec)\r'.format(
+                num_encoded, sps),
             sys.stdout.flush()
     print '{} sentences encoded'.format(num_encoded)
+    np.save(shape_path, np.array([num_encoded, embed_size]))
     sys.stdout.flush()
-    return np.concatenate(embeds, axis=0)
+    return embeds
 
 def compute_sentence_embeds(datadir, restore, savename, max_num, batch_size):
+    part_num = 0
+    embed_path = '{}.dat'.format(savename)
+    shape_path = '{}.shape.npy'.format(savename)
+    if os.path.exists(embed_path):
+        assert os.path.exists(shape_path)
+        shape = tuple(np.load(shape_path))
+        print 'Using precomputed embeddings: {} with shape {}'.format(embed_path, shape)
+        embeds = np.memmap('{}.dat'.format(savename),  dtype=np.float16, mode='r', shape=shape)
+        return embeds
     print 'computing sentence embeddings from model: {}'.format(restore)
-    model, _, data, sess = words.sess_setup(datadir, restore, batch_size=batch_size)
-    embeds = run_loop(sess, data, model.sentence_embeds_2, data.trg.to_tensor(-1), max_num)
-    print embeds.shape
-    np.save('{}.embeds.npy'.format(savename), embeds)
+    model, _, _, sess = words.sess_setup(datadir, restore, batch_size=batch_size)
+    embeds = run_loop(sess, embed_path, shape_path, tf.cast(model.sentence_embeds_2, tf.float16), max_num)
     return embeds
+
+def _make_nearest_k_fn(k):
+    def nearest_k(dist, start):
+        #FIXME breaks if run on a batch with < k vectors
+        indices = np.argpartition(dist, kth=k, axis=1)[:, :k]
+        row_indices = np.repeat(np.arange(dist.shape[0]), k)
+        col_indies = np.reshape(indices, [-1])
+        values = dist[row_indices, col_indies]
+        values = np.reshape(values, [-1, k])
+        return indices, values
+    return nearest_k
+
+def _find_nearest(query_vectors, embeds, n_nearby, batch_size):
+    num_embeds = embeds.shape[0]
+    num_batches = (num_embeds / batch_size) + (1 if num_embeds % batch_size != 0 else 0)
+    nearest_k_fn = _make_nearest_k_fn(k=n_nearby)
+    indices = []
+    dists = []
+    for b in range(num_batches):
+        print 'Running batch {} of {}\r'.format(1 + b, num_batches),
+        sys.stdout.flush()
+        start = b * batch_size
+        end = min(start + batch_size, embeds.shape[0])
+        batch = embeds[start:end, :]
+        batch_indices, batch_dists = zip(*pairwise_distances_chunked(query_vectors, batch,
+            reduce_func=nearest_k_fn, metric='euclidean'))
+        indices.append(np.concatenate(batch_indices) + start)
+        dists.append(np.concatenate(batch_dists))
+    # Combine results from each batch, sort them and take the n_nearby closest
+    indices = np.concatenate(indices, axis=1)
+    dists = np.concatenate(dists, axis=1)
+    sorted_idx = np.argsort(dists, axis=1)[:, :n_nearby]
+    indices = np.concatenate([[row[idx_row]] for row, idx_row in zip(indices, sorted_idx)])
+    dists = np.concatenate([[row[idx_row]] for row, idx_row in zip(dists, sorted_idx)])
+    return indices, dists
 
 def make_similars_examples(embeds, labels, n_nearby, savename):
     """
@@ -73,45 +115,48 @@ def make_similars_examples(embeds, labels, n_nearby, savename):
     tuple to a file that can be used as a set of examples for training. Each line of the file
     contains a tuple of similar sentences separated by: '|SEP|'
     """
+    n_nearby += 1 # The nearest sentence for each example should be itself
     filename = '{}.similars'.format(savename)
     print 'Writing examples to file: {}'.format(filename)
     with open(filename, 'w') as out_file:
-        batch_size = 1024
+        batch_size = 25000
         num_processed = 0
         for batch in range(len(labels)/batch_size):
             query_sentences = labels[batch*batch_size:batch_size + batch*batch_size]
-            vectors = embeds[batch*batch_size:batch_size + batch*batch_size]
-            dists = pairwise_distances(vectors, embeds, 'euclidean')
-            # Use argpartition to quickly get the indices of the closest n sentences
-            nearby_ind = np.argpartition(dists, kth=1 + n_nearby, axis=1)[:, :1 + n_nearby]
-            # Loop over the batch of sentences
-            for ind_row, dist_row, query_sentence in zip(nearby_ind, dists, query_sentences):
-                ind_row = ind_row[np.argsort(dist_row[ind_row])][1:] # sort, remove the query's idx
-                similar_sentences = [query_sentence]
-                similar_sentences.extend([labels[idx] for idx in ind_row])
-                similar_sentences = [s.strip() for s in similar_sentences]
+            query_vectors = embeds[batch*batch_size:batch_size + batch*batch_size]
+            indices, _ = _find_nearest(query_vectors, embeds, n_nearby, batch_size=batch_size)
+            for query_num in range(indices.shape[0]):
+                # write each example (the nearest sentence should be the query itself)
+                similar_sentences = []
+                index_row = indices[query_num, :]
+                for idx in index_row:
+                    similar_sentences.append(labels[idx].strip())
                 out_file.write(' |SEP| '.join(similar_sentences) + '\n')
             num_processed += batch_size
-            if batch % 2 == 0:
-                print '{} lines processed'.format(num_processed)
-                sys.stdout.flush()
+            print '{} examples written'.format(num_processed)
+            sys.stdout.flush()
 
-def show_similar(embeds, label, n_examples=100, n_nearby=4):
-    for query_sentence in random.sample(labels, n_examples):
-        index = labels.index(query_sentence)
-        print index, query_sentence
-        vector = embeds[index, :]
-        dists = pairwise_distances([vector], embeds, 'euclidean')[0]
-        for _ in range(n_nearby):
-            idx = np.argmin(dists)
-            dist = dists[idx]
-            close_sentence = labels[idx]
-            print dist, close_sentence
-            dists[np.argmin(dists)] = np.inf # replace closest with inf to get next-nearest
-        print '--------------------------------------------------'
+def show_similar(embeds, labels, n_examples=10, n_nearby=6):
+    # Gather a random set of queries (sentences embeddings that we'll compare against)
+    query_label_idx = random.randint(0, len(labels) - n_examples)
+    query_vectors = embeds[query_label_idx:query_label_idx+n_examples, :]
+    # Find indices for the embeddings that are nearest to the queries
+    t = time.time()
+    indices, dists = _find_nearest(query_vectors, embeds, n_nearby, batch_size=1000000)
+    print indices.shape, dists.shape
+    t = time.time() - t
+    query_sentences = labels[query_label_idx: query_label_idx + dists.shape[0]]
+    for query_num, query_sentence in enumerate(query_sentences):
+        print "*******************************************************************"
+        print query_sentence
+        dist_row = dists[query_num, :]
+        index_row = indices[query_num, :]
+        for dist, idx in zip(dist_row, index_row):
+            print dist, labels[idx]
+    print 'Took {} seconds ({} s/per query)'.format(t, t/n_examples)
     return
 
-    # TODO: gensim is much faster but doesn't handle duplicate entries very well
+    # TODO: gensim is much faster but doesn't handle duplicate entries very well, and goes OOM
     kv = KeyedVectors(embeds.shape[-1])
     kv.add(labels, embeds)
     random_labels = random.sample(labels, 10)
@@ -125,12 +170,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
     args.write_examples = args.write_examples == 'yes'
     args.project = args.project == 'yes'
-    sentence_embeds = get_sentence_embeds(args.savename, args.labels, args.restore,
-        args.max_num, args.batch_size)
-    labels = open(args.labels).readlines()[:sentence_embeds.shape[0]]
-    show_similar(sentence_embeds, labels)
+    embeds = compute_sentence_embeds(args.labels, args.restore, args.savename, args.max_num,
+        args.batch_size)
+    labels = open(args.labels).readlines()[:embeds.shape[0]]
+    show_similar(embeds, labels)
     if args.write_examples:
-        make_similars_examples(sentence_embeds, labels, n_nearby=4, savename=args.savename)
+        make_similars_examples(embeds, labels, n_nearby=4, savename=args.savename)
     if args.project:
-        projected_embeds = words.project_embeds(args.savename, sentence_embeds)
+        projected_embeds = words.project_embeds(args.savename, embeds)
         words.plot_projections(projected_embeds, labels, args.savename)
